ab3f6faba55b3baec8b3f8c587fb6efe2ba673b4
Christo George <christogeorge@live.in>
2020-10-24 11:12:25 +0200

    Changes bg sub to take total area of contours

--- Lab_Integration/vehicle_model/sensor.py
+++ Lab_Integration/vehicle_model/sensor.py
@@ -10,45 +10,51 @@ lc = lcm.LCM()
 sensor_message = sensor_message()
 
 global last_timestamp
-global curr_timestamp
+global last_obstacle
 global isMoving
 
-last_timestamp = curr_timestamp = 0
+last_timestamp = 0
 isMoving = False
 
 def update_sensor_message(shouldBrake):
-    print("Publish on SENSOR channel")
+    print(f'Publish on SENSOR channel \ {shouldBrake}.')
+    global last_timestamp
     sensor_message.timestamp =  int(time.time()*1000)
     sensor_message.detected_obstacle = shouldBrake
-    lc.publish("SENSOR", sensor_message.encode())
+    if(sensor_message.timestamp - last_timestamp > 10):        
+        lc.publish("SENSOR", sensor_message.encode())
+        last_timestamp = sensor_message.timestamp
+        last_obstacle = sensor_message.detected_obstacle
 
 cap = cv.VideoCapture(0)
 # take first frame of the video
 ret,frame = cap.read()
-backSub = cv.createBackgroundSubtractorMOG2()
+backSub = cv.createBackgroundSubtractorKNN()
 try:
     while True:
         ret, frame = cap.read()
         if ret == True:
             fgMask = backSub.apply(frame)
             cv.imshow('frame', frame)
-            cv.imshow('fgMask', fgMask)
             fgMask = cv.threshold(fgMask, 25, 255, cv.THRESH_BINARY)[1]
             fgMask = cv.erode(fgMask, None, iterations=2, borderType=cv.BORDER_REPLICATE )
+            #fgMask = cv.dilate(fgMask, None, iterations=2, borderType=cv.BORDER_REPLICATE )
+            cv.imshow('fgMask', fgMask)
             contours = cv.findContours(fgMask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
             contours = imutils.grab_contours(contours)
             if(contours == []):
                 update_sensor_message(False)
                 print("No object continue moving")
             else:
+                total_area = 0
                 for cnt in contours:
+                    total_area  = total_area + cv.contourArea(cnt)
                     #print(cv.contourArea(cnt))
-                    if(cv.contourArea(cnt) < 1000 ):
-                        update_sensor_message(False)
-                        continue
-                    else:
-                        #print("Obstacle maann obsatcle")
-                        update_sensor_message(True)
+                if(total_area < 1000 ):
+                    update_sensor_message(False)
+                else:
+                    #print("Obstacle maann obsatcle")
+                    update_sensor_message(True)
                 
             
             k = cv.waitKey(30) & 0xff
