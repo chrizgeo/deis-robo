6a137c00a5e0315d94e174351769a17a3df9e906
Ioannis Broumas <giannibrouma@gmail.com>
2020-12-10 14:35:32 +0200

    added subscriber for cmd

--- dev_ws/src/deis_py_dev/deis_py_dev/sparkie.py
+++ dev_ws/src/deis_py_dev/deis_py_dev/sparkie.py
@@ -23,10 +23,13 @@ class SparkieNode(Node):
         self.subscription = self.create_subscription(String, 'teleop', self.teleop_callback, 10 ) 
         self.subscription = self.create_subscription(String, 'GPS', self.gps_callback, 10 )
         self.subscription = self.create_subscription(String, 'cam_follower', self.cam_callback, 10 )
+        self.subscription = self.create_subscription(String, 'cmd', self.cmd_callback, 10 )
+        
         self.publisher_odom = self.create_publisher(String, 'odom_raw', 50)
         self.publisher_imu = self.create_publisher(String, 'imu_r', 10)
         timer_period = 0.1 # seconds
         self.timer = self.create_timer(timer_period, self.get_sensorData_callback)
+        
         self.ser = serial.Serial(port="/dev/ttyUSB0", baudrate=115200)
         #self.sensor_readings_file = open("data/sensor-readings.csv", "w")
         self.get_logger().info('Node Sparkie initialized!')
@@ -42,6 +45,9 @@ class SparkieNode(Node):
         self.ser.write(msg.data.encode())
         
     def cam_callback(self, msg):
+        self.ser.write(msg.data.encode())
+    
+    def cmd_callback(self, msg):
         self.ser.write(msg.data.encode())
         
     def get_sensorData_callback(self):
