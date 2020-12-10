c96ce18c045d56d4a07bd3530713e330d5ac5335
Christo George <christogeorge@live.in>
2020-12-10 11:17:46 +0100

    updated main.py

--- dev_ws/src/deis_py_dev/deis_py_dev/main.py
+++ dev_ws/src/deis_py_dev/deis_py_dev/main.py
@@ -10,6 +10,7 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from geometry_msgs.msg import Pose
 
 class MainNode(Node):
         def __init__(self):
@@ -17,24 +18,22 @@ class MainNode(Node):
             self.publisher_ = self.create_publisher(String, 'WHEEL_SPEEDS', 10)
             self.teleop_subscription = self.create_subscription(String, 'TELEOP', self.teleop_callback, 10 )
             self.teleop_subscription  # prevent unused variable warning
+            #get the drone pose 
+            self.drone_pose_sub  = self.create_subscription(Pose, '/drone_pose', self.drone_pose_callback, 10)
+            self.drone_pose_sub
             #timer_period = 3
             #self.timer = self.create_timer(timer_period, self.teleop_callback)
             #self.i = 0
             self.get_logger().info('Node Main initialized!')
             
-        ''' def timer_callback(self):
-            msg = String()
-            msg.data = '%d %d' %(self.i, -self.i)
-            self.publisher_.publish(msg)
-            self.get_logger().info('Publishing wheel speeds "%s"' %msg.data)
-            self.i += 10 '''
-            
         def teleop_callback(self,msg):
             msg.data = msg.data + '\n'
             self.publisher_.publish(msg)
             self.get_logger().info('Publishing wheel speeds "%s"' %msg.data)
-            #self.i += 10
-            
+        
+        def drone_pose_callback(self, msg):
+            self.get_logger().info("Got drone pose update")
+        
 
 def main(args=None):
         rclpy.init(args=args)
